922fbd0f3b0f7a8c0634014a7a42a632d2a694f6
Chrystalio <chrystalio.k@outlook.com>
2025-12-12 13:05:44 +0700

    Adjust variant sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -46,7 +46,7 @@ const footerNavItems: NavItem[] = [
 
 export function AppSidebar() {
     return (
-        <Sidebar collapsible="icon" variant="floating">
+        <Sidebar collapsible="icon" variant="inset">
             <SidebarHeader>
                 <SidebarMenu>
                     <SidebarMenuItem>
